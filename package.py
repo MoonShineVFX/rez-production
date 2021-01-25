c1e3cf38553a01ef3825ccda6980f294208a2669
David Lai <davidlatwe@gmail.com>
2021-01-25 20:29:58 +0800

    detach house, add _data for allzpark

--- package.py
+++ package.py
@@ -3,13 +3,13 @@ name = "production"
 
 version = "1.0-m1"
 
-description = "Show production tool provider"
+description = "Production suite tool provider"
 
-requires = [
-    "house-1.2+",
-]
+requires = []
 
 build_command = False
+
+suite_root = "T:/rez-studio/sweet/show"
 
 
 def commands():
@@ -19,8 +19,27 @@ def commands():
     show_eph = ephemerals.get("show")
     if show_eph:
         show_name = show_eph[len(".show-"):]
-    else:
-        show_name = "site-default"
+        suite_path = "{this.suite_root}/%s/bin" % show_name
 
-    suite_path = "$SWEET_SHOW_ROOT/%s/bin" % show_name
-    env.PATH.prepend(suite_path)
+        env.PATH.prepend(suite_path)
+
+    env.PRODUCTION_SUITE_ROOT = "{this.suite_root}"
+
+
+@late()
+def _data():
+    import os
+    this = globals()["this"]
+
+    show_name = this.name
+
+    parts = show_name.split("_")
+    if parts[0].isdigit():
+        parts = parts[1:]
+    pretty_name = " ".join(parts)
+
+    return {
+        "icon": "%s/%s/icon.png" % (this.suite_root, show_name),
+        "label": pretty_name,
+        "category": os.path.basename(this.suite_root),
+    }
